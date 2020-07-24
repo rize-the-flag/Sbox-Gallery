300c9da02f9baee42b05685681b92d6ccf1d266e
Denis <dev.denism@yandex.ru>
2020-07-24 11:50:24 +1000

    minor cleanup

--- src/containers/Gallery.jsx
+++ src/containers/Gallery.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { connect, useDispatch, useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import { ImageCard } from '../components/ImageCard';
 import { getImages } from '../redux/thunks/galleryThunks';
