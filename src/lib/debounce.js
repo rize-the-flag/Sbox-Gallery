8ecfa66f082a0e6ea6d2bd7d20db175ccaabbb16
Denis <dev.denism@yandex.ru>
2020-08-15 19:48:11 +1000

    #8.3

--- src/lib/debounce.js
+++
@@ -1,9 +0,0 @@
-export function debounce ( func, delay ) {
-  let isDelayed = false;
-  return function () {
-    if ( isDelayed )  return;
-    func.apply( this, arguments );
-    isDelayed = true;
-    setTimeout( () => isDelayed = false, delay );
-  };
-}
